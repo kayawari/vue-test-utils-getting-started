b0cc28c109471b5a2bd12d5a906c70cf30c957e7
takayuki.kayawari <kayamelo@gmail.com>
2018-12-25 00:51:41 +0900

    test utils内でのnextTickを使うときの注意点

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 // and the component you want to test
 import { mount } from '@vue/test-utils'
 import Counter from './counter'
+import Vue from 'vue'
 
 describe('Counter', () => {
   // Now mount the component and you have the wrapper
@@ -21,5 +22,33 @@ describe('Counter', () => {
     const button = wrapper.find('button')
     button.trigger('click')
     expect(wrapper.vm.count).toBe(1)
+  })
+})
+
+describe('test Vue.nextTick', () => {
+  // エラーになるのでスキップ
+  xit('will time out', (done) => {
+    Vue.nextTick(() => {
+      expect(true).toBe(false)
+      done()
+    })
+  })
+
+  // NOTE: nextTick内で発生したエラーを補足するには、
+  //       一つはテストのはじめにvueのグローバルエラーハンドラーにdoneコールバックをセットする
+  //       もう一つは引数無しでnextTickにして実行し、Promiseとしてテストランナーに返す。
+  it('will catch the error using done', (done) => {
+    Vue.config.errorHandler = done
+    Vue.nextTick(() => {
+      expect(true).toBe(true)
+      done()
+    })
+  })
+
+  it('will catch the error using a promie', () => {
+    return Vue.nextTick()
+      .then(function () {
+        expect(true).toBe(true)
+      })
   })
 })
